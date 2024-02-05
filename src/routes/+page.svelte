b203b63f4a4cd4989b42fa167ead37676624ad95
pedulli <99214144+pedulli@users.noreply.github.com>
2024-02-05 17:32:28 +0000

    baseline whatever

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<p class="text-4xl">
+	abdc
+</p>
+
+<button class="px-4 py-1 rounded bg-green-500">button</button>
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+  preprocess: [vitePreprocess({})],
+};
